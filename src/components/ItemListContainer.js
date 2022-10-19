7af79199db73a100769bcfeef72a27d1fadf1783
Ezequiel Salvo <salvoezequiel@gmail.com>
2022-10-19 17:23:30 -0300

    Creando componentes

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+function ItemListContainer(){
+return(
+    <div>
+       <h1>¡Hola bienvenidos!</h1>
+    </div>
+);
+}
+
+export default ItemListContainer;
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import CartWidget from "./CartWidget";
+
+function NavBar() {
+
+
+    return (
+        <div>   
+            Menu 1
+            Menu 2
+            Menu 3
+            <CartWidget/>          
+        </div>
+    );
+}
+
+export default NavBar;
