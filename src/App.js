ab98b8e38234132ca4f11a3d31e2c8ed32896690
Ezequiel Salvo <salvoezequiel@gmail.com>
2022-10-26 21:14:15 -0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 function App() {
   return (
+  
     <div>
       <NavBar/>
       <ItemListContainer/> 
     </div>
+
   );
 }
 
